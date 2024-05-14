d005e572918f4a29d87a358fccb6444375123211
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-05-13 22:28:12 -0400

    fazendo a validação com o yup, email repetido e senha com hash

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,20 +1,49 @@
 import { v4 } from "uuid"
 import User from "../models/User"
+import * as Yup from "yup"
 
 class UserController {
   async store(request, response){
+    const schema = Yup.obejct({
+      name : Yup.string().required(),
+      email : Yup.string().email().required(),
+      password : Yup.string().min(6).required(),
+      admin : Yup.boolean(),
+    })
 
-    const { name, email, password_hash, admin } = request.body
+    try{
+      schema.validedSync(request.body, { abortEarly : false })
+    } catch (err) {
+      return response.status(400).json({error : err.errors})
+    }
+
+    const { name, email, password, admin } = request.body
+
+    const userExists = await User.findOne({
+      where : {
+        email,
+      }
+    })
+
+    if(userExists){
+      return response.status(400).json({error : 'User already exist' })
+    }
 
     const user = await User.create({
-      id:v4(),
+      id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     })
-  
-    return response.status(201).json(user)
+
+    return response.status(201).json({
+      id : user.id,
+      name,
+      email,
+      password,
+      admin,
+    })
   }
 }
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,26 @@
 import Sequelize, { Model } from "sequelize"
+import bcrypt from "bcrypt"
 
 class User extends Model {
   static init(sequelize){
     super.init({
       name : Sequelize.STRING,
       email : Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
       password_hash : Sequelize.STRING,
       admin: Sequelize.BOOLEAN,
     }, {
       sequelize
     }
     )
+
+    this.addHook('beforeSave', (user) => { 
+      if(user.password_hash){
+        user.password_hash = bcrypt.hash(user.password_hash, 10)
+      }
+    })
+
+    return this
   }
 }
 
