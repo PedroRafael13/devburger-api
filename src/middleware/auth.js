da2decb814645cc0bc56efe2a88b2580cb78be36
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2024-05-21 10:53:50 -0400

    jwt

---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+import Jwt  from "jsonwebtoken"
+import authConfig from "../config/auth"
+
+
+function authMiddleware(request, response, next){
+  const authToken = request.headers.authorization
+
+  if(!authToken){
+    return response.status(401).json({error : "Token is not provided"})
+  }
+
+  const token = authToken.split(" ").at(1)
+
+  try{
+    Jwt.verify(token, authConfig.secreat, (err, decoded) => {
+      if(!token){
+        throw new Error()
+      }
+      
+      request.userId = decoded.id
+    })
+
+  } catch(err){
+    return response.status(401).json({error : "Token is invalid"})
+  }
+
+  return next()
+}
+
+export default authMiddleware
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import ConfigMulter from "./config/multer"
 import SessionController from "./app/controllers/SessionController"
 import UserController from "./app/controllers/UserController"
 import ProductController from "./app/controllers/ProductController"
+import authMiddleware from "./middleware/auth"
 
 const routes = new Router()
 
@@ -11,8 +12,9 @@ const upload = multer(ConfigMulter)
 
 routes.post('/users', UserController.store )
 routes.post('/session', SessionController.store)
-routes.post('/products', upload.single('file') , ProductController.store)
 
+routes.use(authMiddleware)
+routes.post('/products', upload.single('file') , ProductController.store)
 routes.get('/products', ProductController.index )
 
 export default new Router()
